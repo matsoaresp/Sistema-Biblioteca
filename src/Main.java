a316effb45f5d1961e7723d80ba23b5f5ae48104
Mateus Prado <maateussp@gmail.com>
2025-05-08 13:52:13 -0300

    Modificações

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import aplication.BibliotecaApplication;
 import model.LivroModel;
 import view.BibliotecaView;
-
+//
 public class Main {
     public static void main(String[] args) {
 
         BibliotecaApplication app = new BibliotecaApplication();
         app.run();
+
 
     }
 }
